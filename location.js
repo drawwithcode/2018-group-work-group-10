8e78b96cf1a0c7703a389e5c3434c67a481e80c5
Huang Zixun <42267068+zixuneer@users.noreply.github.com>
2019-02-04 17:33:15 +0100

    Update location.js

--- location.js
+++ location.js
@@ -2,6 +2,8 @@
 var api;
 var myLoc;
 var tra1 = 0;
+var tra2 = 0;
+var tra3 = 0;
 
 var img;
 var s=3;
@@ -25,15 +27,14 @@ function setup() {
   let url =
    //'http://api.waqi.info/feed/geo:45.5109784;9.178321/?token=76257e527f21024547d83e6f3e354caac7d456e8';
    'http://api.waqi.info/feed/geo:' + la + ';' + lo + '/?token=76257e527f21024547d83e6f3e354caac7d456e8';
-  api = loadJSON(url);
-  api = eval(api);
+  loadJSON(url, gotData);
+  //api = eval(api);
 
-  for(var i=0; i<api.length; i++ ){
-    var city = api.data;
+}
 
-  }
-  console.log(city);
-
+function gotData(data){
+  api = data;
+  console.log(api);
 }
 
 
@@ -52,13 +53,49 @@ function draw() {
       s=1;
     }
 
-    //text
-
-    var t = api;
-    text(t, width/2, height/4);
-
     //button
     button();
+
+    //data
+    if(api){
+      var city = api.data.city.name;
+      var idx = api.data.aqi;
+      var pm25 = api.data.iaqi.pm25.v;
+      var pm10 = api.data.iaqi.pm10.v;
+      var time = api.data.time.s;
+    }
+
+    if(frameCount>50){
+    push();
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke(60, tra2);
+    fill(220,tra3);
+    rect(windowWidth/2,  windowHeight/2-83, 420, 200, 5);
+    pop();
+
+    textAlign(LEFT);
+    textFont('Amatic SC');
+    fill(100,tra2);
+    textSize(20);
+    text('You are in: ', windowWidth/2-150, windowHeight/4+15);
+    text('Air Quality Index: ', windowWidth/2-150, windowHeight/4+45);
+    text('PM2.5: ', windowWidth/2-150, windowHeight/4+75);
+    text('PM10: ', windowWidth/2-150, windowHeight/4+105);
+    text('Time: ', windowWidth/2-150, windowHeight/4+135);
+    fill(20,tra2);
+    text(city, windowWidth/2-20, windowHeight/4+15);
+    text(idx, windowWidth/2-20, windowHeight/4+45);
+    text(pm25, windowWidth/2-20, windowHeight/4+75);
+    text(pm10, windowWidth/2-20, windowHeight/4+105);
+    text(time, windowWidth/2-20, windowHeight/4+135);
+    tra2 = tra2 + 3;
+    tra3 = tra3 + 3;
+  }
+
+  if(tra3>190){
+    tra3 = 190;
+  }
 
 }
 
@@ -80,7 +117,7 @@ function button(){
   fill(50, tra1);
   text('HOME PAGE', windowWidth/2,  windowHeight-67);
   if(frameCount>60){
-    tra1 = tra1 + 2;
+    tra1 = tra1 + 1;
   }
   pop();
 
